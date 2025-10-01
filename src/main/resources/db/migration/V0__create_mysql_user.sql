b80b47cc18d807102d548a9987bdd9a72c34f23e
garcia2202 <tonato.thiago@gmail.com>
2025-10-01 19:54:06 -0300

    .

--- src/main/resources/db/migration/V0__create_mysql_user.sql
+++
@@ -1,4 +0,0 @@
--- Criar usuário MySQL para a aplicação
-CREATE USER IF NOT EXISTS 'mottuadmin'@'%' IDENTIFIED BY 'QualiTracker123!';
-GRANT ALL PRIVILEGES ON mottu.* TO 'mottuadmin'@'%';
-FLUSH PRIVILEGES;
